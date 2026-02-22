882390955edd90cdccbfc195621105cb408c478f
onoderra-g <ga19981023ku@gmail.com>
2026-02-22 23:59:30 +0900

    コードフォーマットの調整

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,4 +4,5 @@ utilsパッケージ初期化
 
 from .common import get_base_path, get_executable_dir, get_log_path, get_icon_path
 
-__all__ = ['get_base_path', 'get_executable_dir', 'get_log_path', 'get_icon_path']
+__all__ = ['get_base_path', 'get_executable_dir',
+           'get_log_path', 'get_icon_path']
